7e1639ac9ddc6b6fb7f4c29ae3855ae34751dce5
DastanMega <orm_das@mail.ru>
2024-04-30 11:44:27 +0600

    wrote methods for the first task and checked their operation in the MAIN class

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+import util.FileUtil;
+
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
-
+        String fileDirectory = "src/data/movies.json";
+        try {
+            FileUtil fileRead = new FileUtil(fileDirectory);
+            fileRead.printAllMovies();
+            System.out.println("==============================================================");
+            fileRead.sortMoviesByDirector();
+            System.out.println("==============================================================");
+            fileRead.sortMoviesByDirectorDescending();
+        } catch (IOException io) {
+            io.printStackTrace();
+        }
     }
 }
--- src/util/FileUtil.java
+++ src/util/FileUtil.java
@@ -1,19 +1,84 @@
 package util;
 
 import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+import model.Movie;
 
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.io.FileReader;
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.util.Comparator;
+import java.util.List;
 
 public class FileUtil {
-    private static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
-    private static final Path path;
+    private List<Movie> movies;
 
-    private FileUtil(String filename) {
-        path = Paths.get("data/" + filename + ".json");
+    public FileUtil(String filePath) throws IOException {
+        Gson gson = new Gson();
+        Type movieListType = new TypeToken<List<Movie>>() {
+        }.getType();
+        movies = gson.fromJson(new FileReader(filePath), movieListType);
     }
 
-    public static Movie
+    public void printAllMovies() {
+        for (Movie movie : movies) {
+            System.out.println(movie);
+        }
+    }
+
+    public void searchMoviesByTitle(String title) {
+        for (Movie movie : movies) {
+            if (movie.getName().toLowerCase().contains(title.toLowerCase())) {
+                System.out.println(movie);
+            }
+        }
+    }
+
+    public void sortMoviesByYear() {
+        movies.sort(Comparator.comparingInt(Movie::getYear));
+        printAllMovies();
+    }
+
+    public void sortMoviesByYearDescending() {
+        movies.sort(Comparator.comparingInt(Movie::getYear).reversed());
+        printAllMovies();
+    }
+
+
+    public void sortMoviesByName() {
+        movies.sort(Comparator.comparing(Movie::getName, String.CASE_INSENSITIVE_ORDER));
+        printAllMovies();
+    }
+
+    public void sortMoviesByNameDescending() {
+        movies.sort(Comparator.comparing(Movie::getName, String.CASE_INSENSITIVE_ORDER).reversed());
+        printAllMovies();
+    }
+
+
+    public void sortMoviesByDirector() {
+        movies.sort(new Comparator<Movie>() {
+            @Override
+            public int compare(Movie m1, Movie m2) {
+                return m1.getDirector().getFullName().compareToIgnoreCase(m2.getDirector().getFullName());
+            }
+        });
+
+        printAllMovies();
+    }
+
+    public void sortMoviesByDirectorDescending() {
+        movies.sort(new Comparator<Movie>() {
+            @Override
+            public int compare(Movie m1, Movie m2) {
+                return m2.getDirector().getFullName().compareToIgnoreCase(m1.getDirector().getFullName());
+            }
+        });
+
+        printAllMovies();
+    }
+
+
+
 
 }
